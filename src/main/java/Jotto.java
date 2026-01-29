b6bf88bc247c1f50b24431ea52372493bde33fae
leodebuss <bionerd729@gmail.com>
2026-01-29 11:32:22 -0800

    Declaration, not implementation of methods & variables

--- src/main/java/Jotto.java
+++ src/main/java/Jotto.java
@@ -1,7 +1,64 @@
+import java.util.ArrayList;
+
 /**
- * @author feng3302
+ * @author Leonardo Lopez
  * @version 0.1.0
  * @Since 1/29/26
  **/
 public class Jotto {
+    private int WORD_SIZE;
+    private int score;
+    private boolean DEBUG;
+    private String currentWord;
+    private String filename;
+    private ArrayList<String> playGuesses;
+    private ArrayList<String> playWords;
+    private ArrayList<String> wordList;
+
+    public boolean pickWord() {
+
+    }
+    public String showWordList() {
+
+    }
+
+    public ArrayList<String> showPlayerGuesses() {
+
+    }
+    void playerGuessScores(ArrayList<String> playGuesses) {
+
+    }
+    public void setCurrentWord(String currentWord) {
+
+    }
+
+    public ArrayList<String> readWords() {
+
+    }
+    public void play() {
+
+    }
+    int guess() {
+
+    }
+
+    public ArrayList<String> getPlayWords() {
+
+    }
+
+    public String getCurrentWord() {
+
+    }
+    public int getLetterCount(String temp1) {
+
+    }
+    public String showPlayedWords() {
+
+    }
+    public boolean addPlayerGuess(String temp2) {
+
+    }
+    void updateWordList() {
+
+    }
 }
